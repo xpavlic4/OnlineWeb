86a1a8fee05ada920d3b9e318b3825a28fc66c5f
Radim Pavlicek <Radim.Pavlicek@focusmr.com>
2013-02-21 16:59:04 +0100

    refs #4550 - rest impl

--- OnlineWeb-Web/src/main/java/com/focusmr/online/onlineweb/rest/ParameterResourceRESTService.java
+++ OnlineWeb-Web/src/main/java/com/focusmr/online/onlineweb/rest/ParameterResourceRESTService.java
@@ -1,19 +1,20 @@
 package com.focusmr.online.onlineweb.rest;
 
+import com.focusmr.online.db.ConnectionFactoryBase;
 import com.focusmr.online.onlineweb.api.Parameter;
-import com.focusmr.online.onlineweb.model.App;
+import com.focusmr.online.userdb.PrefsBase;
+import com.focusmr.online.userdb.ReadParam;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Collections;
-import java.util.List;
+import java.sql.SQLException;
 
 /**
  * JAX-RS Example
@@ -27,19 +28,19 @@ public class ParameterResourceRESTService {
     private EntityManager em;
 
     @GET
-    @Path("/params")
-    @Produces(MediaType.TEXT_XML)
-    public List<Parameter> listAllParameters(@PathParam("userId") String userId) {
-//       com.focusmr.online.beans.User.switchToUser(userName, false, applicationId);
-        return Collections.emptyList();
-    }
-
-    @GET
     @Path("/params/{name}")
     @Produces(MediaType.TEXT_XML)
-    public List<App> lookupMemberById(@PathParam("name") long name) {
-
-        final TypedQuery<App> query = em.createQuery("select a from App a", App.class);
-        return query.getResultList();
+    public Parameter listAllParameters(@PathParam("userId") String aUserId,
+                                       @PathParam("appId") String aApplicationId,
+                                       @PathParam("country") String aCountry, @PathParam("name") String aName
+    ) throws SQLException, NamingException {
+        final int userId = Integer.parseInt(aUserId);
+        final int applicationId = Integer.parseInt(aApplicationId);
+        final int countryId = Integer.parseInt(aCountry);
+        final PrefsBase prefsBase = new PrefsBase(userId, 1, applicationId, countryId, new ConnectionFactoryBase());
+        final ReadParam param = prefsBase.getParam(aName);
+        return new Parameter(aName, param.getValue().toString());
     }
+
+
 }
