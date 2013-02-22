d8075ddf961f5f71f1b19dbe19a38aaa2af46d7c
Radim Pavlicek <Radim.Pavlicek@focusmr.com>
2013-02-22 09:16:18 +0100

    refs #4550 - belongs to property

--- OnlineWeb-Web/src/main/java/com/focusmr/online/onlineweb/model/UserTable.java
+++ OnlineWeb-Web/src/main/java/com/focusmr/online/onlineweb/model/UserTable.java
@@ -3,6 +3,7 @@ package com.focusmr.online.onlineweb.model;
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -11,8 +12,13 @@ import javax.xml.bind.annotation.XmlRootElement;
 @javax.persistence.Table(name = "USER_TABLE", schema = "ONLINE_AUTH")
 @Entity
 @XmlRootElement
+@NamedQuery(name = UserTable.USER_BYID, query = "select a from UserTable a where a.userId = :id")
 public class UserTable {
+    public static final String USER_BYID = "UserTable.byId";
     private int userId;
+
+
+    private int belongsTo;
 
     @javax.persistence.Column(name = "USER_ID")
     @Id
@@ -36,6 +42,16 @@ public class UserTable {
         this.userName = userName;
     }
 
+    @javax.persistence.Column(name = "BELONGS_TO")
+    @Basic
+    public int getBelongsTo() {
+        return belongsTo;
+    }
+
+    public void setBelongsTo(int belongsTo) {
+        this.belongsTo = belongsTo;
+    }
+
     @SuppressWarnings("RedundantIfStatement")
     @Override
     public boolean equals(Object o) {
@@ -44,8 +60,9 @@ public class UserTable {
 
         UserTable userTable = (UserTable) o;
 
+        if (belongsTo != userTable.belongsTo) return false;
         if (userId != userTable.userId) return false;
-        if (userName != null ? !userName.equals(userTable.userName) : userTable.userName != null) return false;
+        if (!userName.equals(userTable.userName)) return false;
 
         return true;
     }
@@ -53,7 +70,8 @@ public class UserTable {
     @Override
     public int hashCode() {
         int result = userId;
-        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + belongsTo;
+        result = 31 * result + userName.hashCode();
         return result;
     }
 }
