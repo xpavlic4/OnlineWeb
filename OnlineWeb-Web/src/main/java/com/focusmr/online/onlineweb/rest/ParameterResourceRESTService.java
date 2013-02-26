00d02259cd856381c9f999c1cc13c1e394d2c3d1
Radim Pavlicek <Radim.Pavlicek@focusmr.com>
2013-02-26 12:39:48 +0100

    refs #4550 - documentation for users/.../param rest

--- OnlineWeb-Web/src/main/java/com/focusmr/online/onlineweb/rest/ParameterResourceRESTService.java
+++ OnlineWeb-Web/src/main/java/com/focusmr/online/onlineweb/rest/ParameterResourceRESTService.java
@@ -17,7 +17,7 @@ import java.sql.SQLException;
 /**
  * Gets pramatere value for specified user, app and country.
  */
-@Path("/users/{userId}/apps/{appId}/countries/{country}/")
+@Path("/users/{userId}/apps/{appId}/countries/{countryId}/")
 @RequestScoped
 public class ParameterResourceRESTService {
 
@@ -27,34 +27,30 @@ public class ParameterResourceRESTService {
     /**
      * Gets evaluated parameter.
      *
-     * @param aUserId        id of user
-     * @param aApplicationId id of applicatoin
-     * @param aCountry       id of country
-     * @param aName          name of parameter
+     * @param userId    user id
+     * @param appId     id of applicatoin
+     * @param countryId id of country
+     * @param name      name of parameter
      * @return parameter key and value
      * @throws SQLException
      * @throws NamingException E.g. http://js2:9090/OnlineWeb-Web/rest/version1/users/1/apps/1/countries/1/params/locale
      *                         <pre> {@code
-     *                         <parameter>
-     *                            <key>locale</key>
-     *                           <value>en_GB</value>
-     *                         </parameter>
-     *                         }
-     *                         </pre>
+     *                                                 <parameter>
+     *                                                    <key>locale</key>
+     *                                                   <value>en_GB</value>
+     *                                                 </parameter>
+     *                                                 }
+     *                                                 </pre>
      */
     @GET
     @Path("/params/{name}")
-    @Produces(MediaType.TEXT_XML)
-    public Parameter listAllParameters(@PathParam("userId") String aUserId,
-                                       @PathParam("appId") String aApplicationId,
-                                       @PathParam("country") String aCountry, @PathParam("name") String aName
+    @Produces({MediaType.TEXT_XML, MediaType.APPLICATION_JSON})
+    public Parameter listAllParameters(@PathParam("userId") Integer userId,
+                                       @PathParam("appId") Integer appId,
+                                       @PathParam("countryId") Integer countryId, @PathParam("name") String name
     ) throws SQLException, NamingException {
 
-        final int userId = Integer.parseInt(aUserId);
-        final int applicationId = Integer.parseInt(aApplicationId);
-        final int countryId = Integer.parseInt(aCountry);
-
-        ParamQueryObject o = ParamQueryObject.builder().userId(userId).applicationId(applicationId).countryId(countryId).name(aName).build();
+        ParamQueryObject o = ParamQueryObject.builder().userId(userId).applicationId(appId).countryId(countryId).name(name).build();
         return resolver.process(o);
     }
 
