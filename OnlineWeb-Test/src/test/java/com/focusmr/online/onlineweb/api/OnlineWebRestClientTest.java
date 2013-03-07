a0b051b2e7fcf84af7ad6c472d97606a57ffe7f6
Radim Pavlicek <Radim.Pavlicek@focusmr.com>
2013-03-07 11:34:32 +0100

    #4659 - preparation for acceptance use-cases

---
+++ OnlineWeb-Test/src/test/java/com/focusmr/online/onlineweb/api/OnlineWebRestClientTest.java
@@ -0,0 +1,45 @@
+package com.focusmr.online.onlineweb.api;
+
+import com.google.common.base.Strings;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.Properties;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ */
+public class OnlineWebRestClientTest {
+
+    private final String deployUrl = "deployUrl";
+
+    private Properties readProperties() throws IOException {
+        InputStream resourceAsStream = OnlineWebRestClientTest.class.getResourceAsStream("/settings.properties");
+        Properties properties = new Properties();
+        properties.load(resourceAsStream);
+        return properties;
+    }
+
+    @Test
+    public void shouldReadTestRestURL() throws IOException {
+        final Properties properties = readProperties();
+        assertTrue(properties != null);
+        final String deployUrl = properties.getProperty(this.deployUrl);
+        assertTrue(null != deployUrl && !deployUrl.isEmpty());
+    }
+
+    @Test
+    @Ignore("we cannot test version that wasn't deployed yet")
+    public void shouldCallWebServiceWithExpectedResults() throws IOException {
+        final Properties properties = readProperties();
+        final OnlineWebRestClient client = new OnlineWebRestClient(URI.create(properties.getProperty(deployUrl)).toURL());
+
+        final OnlineWebRestClient.Builder b = new OnlineWebRestClient.Builder().application(1).country(1).param("locale").userId(1);
+        final Parameter parameter = client.resolveParam(b);
+        assertTrue(!Strings.isNullOrEmpty(parameter.getValue()));
+    }
+}
