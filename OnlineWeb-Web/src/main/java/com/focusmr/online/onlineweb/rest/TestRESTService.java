d8ead205e006410e0d64f6908fd9fe6888b23184
Radim Pavlicek <Radim.Pavlicek@focusmr.com>
2013-03-08 11:40:46 +0100

    #4659 - smoke test produces text/plain

--- OnlineWeb-Web/src/main/java/com/focusmr/online/onlineweb/rest/TestRESTService.java
+++ OnlineWeb-Web/src/main/java/com/focusmr/online/onlineweb/rest/TestRESTService.java
@@ -3,6 +3,8 @@ package com.focusmr.online.onlineweb.rest;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.Date;
 
 /**
@@ -17,6 +19,7 @@ public class TestRESTService {
      * @return string representation of current date  e.g. Tue Feb 26 10:39:38 CET 2013
      */
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
     public String printDate() {
         return new Date().toString();
     }
